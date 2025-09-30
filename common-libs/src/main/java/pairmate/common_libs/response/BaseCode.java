bebfe1854b0505d388b85755b9931570b59501d0
jiwonLee42 <ljw56b@gmail.com>
2025-10-01 03:11:21 +0900

    [FIX] import 문 수정

--- common-libs/src/main/java/pairmate/common_libs/response/BaseCode.java
+++ common-libs/src/main/java/pairmate/common_libs/response/BaseCode.java
@@ -1,6 +1,6 @@
 package pairmate.common_libs.response;
 
-import org.apache.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 
 public interface BaseCode {
     HttpStatus getHttpStatus();
