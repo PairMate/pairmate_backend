8bc3410cd03399602866325c8b9bae325a89f051
ian <hope030327@naver.com>
2025-10-21 00:37:55 +0900

    [REFACTOR] 리뷰 관련 예외처리 추가

--- review-service/src/main/java/pairmate/review_service/exception/ReviewExceptionHandler.java
+++ review-service/src/main/java/pairmate/review_service/exception/ReviewExceptionHandler.java
@@ -6,9 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pairmate.common_libs.exception.CustomException;
 import pairmate.common_libs.response.ApiResponse;
 import pairmate.common_libs.response.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestControllerAdvice
 public class ReviewExceptionHandler {
 
@@ -57,5 +60,26 @@ public class ReviewExceptionHandler {
         errorMessage = "외부 서비스 호출 중 오류가 발생했습니다.";
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.onFailure(ErrorCode.INTERNAL_SERVER_ERROR, errorMessage));
+    }
+
+    /**
+     * CustomException을 처리하는 핸들러
+     */
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Object>> handleCustomException(CustomException e) {
+
+        ErrorCode errorCode = (ErrorCode) e.getErrorCode();
+
+        // 2. 표준 ApiResponse 에러 본문을 만듭니다.
+        ApiResponse<Object> body = ApiResponse.builder()
+                .isSuccess(false)
+                .code(errorCode.getCode())
+                .message(e.getMessage())
+                .result(null)
+                .build();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(body);
     }
 }
